3e6c2563520317aaa9c7fb0b252bb9f3167d5bee
kadas36 <oduorthomas14@gmail.com>
2020-11-29 21:55:51 +0300

    Add Source view

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .requests import get_sources
 
 # Views
 @app.route('/')
@@ -8,12 +9,15 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    #Getting General sources
+    general_sources = get_sources('general')
+    print(general_sources)
+    title = 'Home-NEWS sources'
+    return render_template('index.html', title=title, general_sources=general_sources)
 
-@app.route('/news/<news_id>')
-def news(news_id):
-
-    '''
-    View news page function that returns the news details page and its data
-    '''
-    return render_template('news.html',id = news_id)    
+# @app.route('/source/<source_id>')
+# def source(source_id):
+#     '''
+#     view source page function that returns the source and its data
+#     '''
+#     return render_template('source.html', id = source_id)     
